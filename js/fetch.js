75df850772fcd8f12048721edec684e4d70b7c81
Nico Gufron <nicogufron@gmail.com>
2019-06-24 23:04:26 +0700

    Added forecast for tomorrow

--- js/fetch.js
+++ js/fetch.js
@@ -4,41 +4,42 @@ window.onload = function(){
     var city = "Surabaya";
     var btn = document.getElementById("qkota");
 
-    var urlapixu = "https://api.apixu.com/v1/forecast.json?key=" + key + "&q=" + city + "&callback=?";
+    var urlapixu = "https://api.apixu.com/v1/forecast.json?key=" + key + "&q=" + city + "&days=7";
     fetch(urlapixu).then((resp)=>resp.json()).then(function(data){
             console.log(data);
             document.getElementById("city").innerHTML = data.location.name + ", " + data.location.country;
-            document.getElementById("weather").innerHTML = data.current.condition.text;
-            document.getElementById("weatherpic").innerHTML = "<img src='http:" + data.current.condition.icon + "' width='100'>";
-            document.getElementById("suhu").innerHTML = " " + data.current.temp_c;
-            document.getElementById("suhuf").innerHTML = " " + data.current.temp_f;
-            document.getElementById("windspeed").innerHTML = " " + data.current.wind_kph;
-            document.getElementById("compass").innerHTML = " " + data.current.wind_dir;
-            document.getElementById("precipitation").innerHTML = " " + data.current.precip_in;
-            document.getElementById("humid").innerHTML = " " + data.current.humidity;
-            document.getElementById("uv").innerHTML = " " + data.current.uv;
-            document.getElementById("visual").innerHTML = " " + data.current.vis_km;
-            document.getElementById("sunrise").innerHTML = " " + data.forecast.forecastday[0].astro.sunrise;
-            document.getElementById("sunset").innerHTML = " " + data.forecast.forecastday[0].astro.sunset;
+            document.getElementById("weather").innerHTML = data.forecast.forecastday[1].day.condition.text;
+            document.getElementById("weatherpic").innerHTML = "<img src='http:" + data.forecast.forecastday[1].day.condition.icon + "' width='100'>";
+            document.getElementById("suhu").innerHTML = " " + data.forecast.forecastday[1].day.maxtemp_c;
+            document.getElementById("suhuf").innerHTML = " " + data.forecast.forecastday[1].day.maxtemp_f;
+            document.getElementById("precipitation").innerHTML = " " + data.forecast.forecastday[1].day.totalprecip_in;
+            document.getElementById("humid").innerHTML = " " + data.forecast.forecastday[1].day.avghumidity;
+            document.getElementById("uv").innerHTML = " " + data.forecast.forecastday[1].day.uv;
+            document.getElementById("visual").innerHTML = " " + data.forecast.forecastday[1].day.avgvis_km;
+            document.getElementById("sunrise").innerHTML = " " + data.forecast.forecastday[1].astro.sunrise;
+            document.getElementById("sunset").innerHTML = " " + data.forecast.forecastday[1].astro.sunset;
+            document.getElementById("tanggal").innerHTML = " " + data.forecast.forecastday[1].date;
         }).catch(function(error){
             console.log(error);
     });
     btn.onclick = function(){
         city = document.getElementById("kota").value;
-        var urlapixu = "https://api.apixu.com/v1/forecast.json?key=" + key + "&q=" + city + "&callback=?";
+        var urlapixu = "https://api.apixu.com/v1/forecast.json?key=" + key + "&q=" + city + "&days=7";
         fetch(urlapixu).then((resp)=>resp.json()).then(function(data){
             console.log(data);
+            
             document.getElementById("city").innerHTML = data.location.name + ", " + data.location.country;
-            document.getElementById("weather").innerHTML = data.current.condition.text;
-            document.getElementById("weatherpic").innerHTML = "<img src='http:" + data.current.condition.icon + "' width='100'>";
-            document.getElementById("suhu").innerHTML = " " + data.current.temp_c;
-            document.getElementById("suhuf").innerHTML = " " + data.current.temp_f;
-            document.getElementById("windspeed").innerHTML = " " + data.current.wind_kph;
-            document.getElementById("compass").innerHTML = " " + data.current.wind_dir;
-            document.getElementById("precipitation").innerHTML = " " + data.current.precip_in;
-            document.getElementById("humid").innerHTML = " " + data.current.humidity;
-            document.getElementById("uv").innerHTML = " " + data.current.uv;
-            document.getElementById("visual").innerHTML = " " + data.current.vis_km;
+            document.getElementById("weather").innerHTML = data.forecast.forecastday[1].day.condition.text;
+            document.getElementById("weatherpic").innerHTML = "<img src='http:" + data.forecast.forecastday[1].day.condition.icon + "' width='100'>";
+            document.getElementById("suhu").innerHTML = " " + data.forecast.forecastday[1].day.maxtemp_c;
+            document.getElementById("suhuf").innerHTML = " " + data.forecast.forecastday[1].day.maxtemp_f;
+            document.getElementById("precipitation").innerHTML = " " + data.forecast.forecastday[1].day.totalprecip_in;
+            document.getElementById("humid").innerHTML = " " + data.forecast.forecastday[1].day.avghumidity;
+            document.getElementById("uv").innerHTML = " " + data.forecast.forecastday[1].day.uv;
+            document.getElementById("visual").innerHTML = " " + data.forecast.forecastday[1].day.avgvis_km;
+            document.getElementById("sunrise").innerHTML = " " + data.forecast.forecastday[1].astro.sunrise;
+            document.getElementById("sunset").innerHTML = " " + data.forecast.forecastday[1].astro.sunset;
+            document.getElementById("tanggal").innerHTML = " " + data.forecast.forecastday[1].date;
         }).catch(function(error){
             console.log(error);
     })};
