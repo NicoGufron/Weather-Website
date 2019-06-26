7740c990f45a6b0b4c9f3037ec714603f1c8242d
Yoel <m26416070@john.petra.ac.id>
2019-06-26 11:49:05 +0700

    Final

--- js/fetch.js
+++ js/fetch.js
@@ -40,6 +40,19 @@ window.onload = function(){
             document.getElementById("sunrise").innerHTML = " " + data.forecast.forecastday[1].astro.sunrise;
             document.getElementById("sunset").innerHTML = " " + data.forecast.forecastday[1].astro.sunset;
             document.getElementById("tanggal").innerHTML = " " + data.forecast.forecastday[1].date;
+
+            localStorage.setItem("City", data.location.name + ", " + data.location.country);
+            localStorage.setItem("Weather", data.forecast.forecastday[1].day.condition.text);
+            localStorage.setItem("Weather Picture", "<img src='http:" + data.forecast.forecastday[1].day.condition.icon + "'width='100'>");
+            localStorage.setItem("Max Temp C", data.forecast.forecastday[1].day.maxtemp_c);
+            localStorage.setItem("Max Temp F", data.forecast.forecastday[1].day.maxtemp_f);
+            localStorage.setItem("Precipitation", data.forecast.forecastday[1].day.totalprecip_in);
+            localStorage.setItem("Humidity", data.forecast.forecastday[1].day.avghumidity);
+            localStorage.setItem("UV", data.forecast.forecastday[1].day.uv);
+            localStorage.setItem("Avg Visual", data.forecast.forecastday[1].day.avgvis_km);
+            localStorage.setItem("Sunrise", data.forecast.forecastday[1].astro.sunrise);
+            localStorage.setItem("Sunset", data.forecast.forecastday[1].astro.sunset);
+            localStorage.setItem("Date", data.forecast.forecastday[1].date);
         }).catch(function(error){
             console.log(error);
     })};
